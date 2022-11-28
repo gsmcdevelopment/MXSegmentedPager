cd53e9e861a847e5a324ec2a763d5432153f9ecb
gsmcdevelopment <110977890+gsmcdevelopment@users.noreply.github.com>
2022-11-28 12:37:17 +0530

    Package.swift changes

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
                  targets: ["MXSegmentedPager"])
     ],
     dependencies: [
-        .package(url: "https://github.com/maxep/MXPagerView.git", from: "0.2.0"),
+        .package(url: "https://github.com/DharaniSadasivam/MXPagerViewReloadUpdate.git", from: "1.0.0"),
         .package(url: "https://github.com/maxep/MXSegmentedControl.git", from: "1.1.0"),
         .package(url: "https://github.com/maxep/MXParallaxHeader.git", from: "1.0.0"),
     ],
@@ -17,7 +17,7 @@ let package = Package(
         .target(
             name: "MXSegmentedPager",
             dependencies: [
-                "MXPagerView",
+                "MXPagerViewReloadUpdate",
                 "MXSegmentedControl",
                 "MXParallaxHeader"
             ], path: "MXSegmentedPager"
